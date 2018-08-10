9216705b0e7bf48949369c1ac836371dca1ae012
Giovanni Tizzoni <giovanni.tizzoni@gmail.com>
2018-08-10 09:26:38 +0200

    [ADD] Calculation iddittalegata

--- datasources/ma_anagrafiche/ditte_calculations.js
+++ datasources/ma_anagrafiche/ditte_calculations.js
@@ -1,4 +1,12 @@
 /**
+ * @properties={type:8,typeid:36,uuid:"8C8AE714-C3BC-49D5-BAF7-F3854B159254"}
+ */
+function iddittalegata()
+{
+	return tipologia != globals.Tipologia.STANDARD ? ditte_to_ditte_legami.iddittariferimento : idditta;
+}
+
+/**
  * @properties={type:12,typeid:36,uuid:"5EFBE03A-CB14-4907-8922-74D97AB02FAF"}
  */
 function desc_estero()
